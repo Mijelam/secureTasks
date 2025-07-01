8fcf90cce908b491b898a95811633322f81abb3e
Atse <MiguelAngelo_lll@hotmail.com>
2025-07-01 00:46:22 -0500

    Quick Fix

--- securetasks/settings.py
+++ securetasks/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cw6hi_w&)tjm69r=#tn45dwl*m4rq*tm1uk%2f-q$hsxg5!n33'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from .settings_local import *
